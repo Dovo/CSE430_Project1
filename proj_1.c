99b25a1b5cfcdb8a5b1b1610bbb7b96cb9ef67ef
Miles <miles.laff@gmail.com>
2014-03-24 16:37:46 -0700

    All assignment 1 functions working correctly!

--- proj_1.c
+++ proj_1.c
@@ -3,47 +3,100 @@
 void expect(int expected, int actual)
 {
 	if(expected == actual)
-	{  
+	{
 		printf("Passed test. Expected %d, got %d.\n", expected, actual);
 	}
-
-	else 
-	{ 
+    
+	else
+	{
 		printf("Failed test. Expected %d, got %d.\n", expected, actual);
 	}
 }
 
-/* test initqueue and correct payload*/
-/*void test1()
+/*
+ 
+ void InitTest()
+ {
+ item *head;
+ InitQueue(&head);
+ item test = NewItem();
+ test->payload = 2;
+ AddQueue(&head, test);
+ int x = test->payload;
+ 
+ expect(2, x);
+ }
+
+void AddTest()
 {
-	item *head;
+	item head;
 	InitQueue(&head);
+	
 	item test = NewItem();
 	test->payload = 2;
-	AddQueue(&head, test);
-	int x = test->payload;
-
-	expect(2, x);
-}*/
-
-void test2()
-{
-	item *head;
-	InitQueue(&head);
-	item test = NewItem();
-	test->payload = 2;
-
+    
 	item test2 = NewItem();
 	test2->payload = 5;
-
+    
+    item test3 = NewItem();
+    test3->payload = 7;
+    
 	AddQueue(&head, test);
 	AddQueue(&head, test2);
+    AddQueue(&head, test3);
 }
 
+void DeleteTest()
+{
+    item head;
+	InitQueue(&head);
+	
+	item test = NewItem();
+	test->payload = 2;
+    
+	item test2 = NewItem();
+	test2->payload = 5;
+    
+    item test3 = NewItem();
+    test3->payload = 7;
+    
+	AddQueue(&head, test);
+	AddQueue(&head, test2);
+    AddQueue(&head, test3);
+
+    DelQueue(&head);
+}
+ 
+*/
+
+void RotateTest()
+{
+    item head;
+	InitQueue(&head);
+	
+	item test = NewItem();
+	test->payload = 2;
+    
+	item test2 = NewItem();
+	test2->payload = 5;
+    
+    item test3 = NewItem();
+    test3->payload = 7;
+    
+	AddQueue(&head, test);
+	AddQueue(&head, test2);
+    AddQueue(&head, test3);
+    
+    RotateQ(&head);
+    RotateQ(&head);
+}
 
 int main()
 {
-	test2();
-
+	//InitTest();
+    //AddTest();
+    //DeleteTest();
+    RotateTest();
+    
 	return 0;
 }
--- q.h
+++ q.h
@@ -4,17 +4,18 @@
 
 struct q
 {
-	int payload;
+    int payload;
 	struct q *next;
 	struct q *prev;
 };
 
-typedef struct q *item;
+typedef struct q* item;
 
 //default constructor sets payload to 0
 item NewItem()
 {
-	item p = malloc(sizeof(item));
+	item p = malloc(sizeof(struct q));
+printf("%lu ", sizeof(struct q));
 	p->payload = 0;
 	p->next = NULL;
 	p->prev = NULL;
@@ -22,51 +23,63 @@ item NewItem()
 }
 
 //set head pointer to point to NULL
-void InitQueue(item **head)
+void InitQueue(item *head)
 {
-	*head = NULL;
+    *head = NULL;
 }
 
-void AddQueue(item **head, item p)
+void AddQueue(item *head, item p)
 {
-	printf("in add\n");
+    printf("\n\nin add\n");
+    
+	//if head points to null, set head to point to p
 	if(*head == NULL)
 	{
-		printf("head is null\n");
-		*head = &p;
-
-		/*
-		(*head)->next = *head;
-		(*head)->prev = *head;
-		*/
-
+printf("head is null\n");
+		//head now points to memory location of p
+        *head = p;
+        
 		p->next = p;
 		p->prev = p;
+        
+		printf("after set\n");
+		return;
+	}
+    
+	//if queue has 1 node, add after that node
+	else if(((*head)->next) == (*head))
+	{
+printf("only one node\n");
+		(*head)->next = p;
+        p->next = *head;
+        p->prev = *head;
+        
+        (*head)->prev = p;
 		return;
 	}
 
+	//if >1 node, iterate to end of list and add then p
 	else
 	{
-		printf("over here\n");
-
-		item temp = **head;
-
-		while(temp->next != **head)
+printf("over here\n");
+        
+		item temp = *head;
+        
+		while(temp->next != *head)
 		{
-			printf("im in the loop yo!\n");
-
-			if(temp->next == NULL)
-			{
-				printf("next is NULL\n");
-			}
+printf("im in the loop yo!\n");
 			temp = temp->next;
 		}
-
-		printf("out of loop\n");
-		temp->next = p;
-
+        
+printf("out of loop\n");
+		
+        //set next item as p, set p->prev to original last item, and
+        //p->next to head pointer
+        temp->next = p;
 		p->prev = temp;
-		p->next = **head;
+		p->next = *head;
+        
+        (*head)->prev = p;
 		return;
 	}
 }
@@ -79,32 +92,34 @@ item DelQueue(item *head)
 		printf("Error: could not delete, queue is empty\n");
 		return *head;
 	}
-
-	//if queue has 1 node, delete that node
-	else if(&(*head)->next == &(*head))
+    
+	//if queue has 1 node, delete that node and return it
+	else if((*head)->next == (*head))
 	{
-		*head = NULL;
 		item temp = *head;
+        *head = NULL;
 		return temp;
 	}
-
+    
+	//if queue has >1 node, remove head and update head to point to next
 	else
 	{
-		item temp = *head;
-
-		while(temp->next != *head)
-		{
-			temp = temp->next;
-		}
-
-		temp->next = (*head)->next;
-		item temp2 = temp->next;
-		temp2->prev = temp;
-		return *head;
+        //save head and head's prev pointer (last item in queue)
+        item temp = *head;
+        item savePrev = (*head)->prev;
+        
+        //let head point to next node and let head->prev point to previous last element
+        *head = temp->next;
+        (*head)->prev = savePrev;
+        
+        //update prev item to point to new head
+        savePrev->next = *head;
+        
+        return temp;
 	}
 }
 
-void RotateQ(item **head)
+void RotateQ(item *head)
 {
-	**head = (**head)->next;
+	*head = (*head)->next;
 }
